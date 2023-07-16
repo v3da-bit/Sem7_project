489a6a6aa9d081e93f30fbdde7d05c2d5e487edf
v3da-bit <vedantkhamar975@gmail.com>
2023-07-16 23:54:16 +0530

    semi-final1

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -69,7 +69,7 @@ function NAvBar() {
           <li>
             <a href='' onClick={() => {
               navigate('/signup')
-            }} className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Register</a>
+            }} className={style1}>Register</a>
           </li>
         </>
       )
